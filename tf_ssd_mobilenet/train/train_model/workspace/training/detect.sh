3b107479b2ed6046224b30b941c64d4c845e2a6b
Wei Chen <cweinone@gmail.com>
2020-07-13 20:34:36 -0400

    prepare for demo

--- tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=1
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
-BASE_PATH=../../../Datasets/CMU/Preprocessing/
+BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU/Preprocessing/
 TARGET_PATH=setxx1
 python detector.py ${BASE_PATH} \
                     ${TARGET_PATH} \
