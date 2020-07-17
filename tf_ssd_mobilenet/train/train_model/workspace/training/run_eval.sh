8c7f3f069c759598dc6dd5d2fc6662d21b05844c
Wei Chen <cweinone@gmail.com>
2020-07-17 13:52:38 -0400

    Prepare for evaluation fast rcnn

--- tf_ssd_mobilenet/train/train_model/workspace/training/run_eval.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/run_eval.sh
@@ -1,3 +1,5 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
-python eval.py --eval_dir=./eval/eval_ssd_mobilenet_v1_FLIR_CMU00-05/ --checkpoint_dir=./training/train_ssd_mobilenet_v1_FLIR_CMU00-05/ --pipeline_config_path=./configs/ssd_mobilenet_v1.config
+python eval.py --eval_dir=./eval/eval_faster_rcnn_inception_resnet_v2_FLIR_CMU00-05/ \
+                --checkpoint_dir=./training/train_faster_rcnn_inception_resnet_v2_FLIR_CMU00-05/ \
+                --pipeline_config_path=./configs/faster_rcnn_inception_resnet_v2_atrous_coco.config
