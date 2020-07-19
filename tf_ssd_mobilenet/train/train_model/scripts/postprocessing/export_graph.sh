c24719c793741786e3d3556cdc46145a9db8a16e
Wei Chen <cweinone@gmail.com>
2020-07-19 17:09:03 -0400

    Prepare for confusion matrix FLIR+cmu00-05

--- tf_ssd_mobilenet/train/train_model/scripts/postprocessing/export_graph.sh
+++ tf_ssd_mobilenet/train/train_model/scripts/postprocessing/export_graph.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 INPUT_TYPE=image_tensor
-PIPELINE_CONFIG_PATH=../../workspace/training/training/train_ssd_mobilenet_v1_FLIR_CMU05/pipeline.config
-TRAINED_CKPT_PREFIX=../../workspace/training/training/train_ssd_mobilenet_v1_FLIR_CMU05/model.ckpt-400124
-EXPORT_DIR=../../workspace/training/exported_models/froze_graph_0719/
+PIPELINE_CONFIG_PATH=../../workspace/training/training/train_ssd_mobilenet_v1_FLIR_CMU00-05/pipeline.config
+TRAINED_CKPT_PREFIX=../../workspace/training/training/train_ssd_mobilenet_v1_FLIR_CMU00-05/model.ckpt-523910
+EXPORT_DIR=../../workspace/training/exported_models/froze_graph_0720/
 python export_inference_graph.py \
     --input_type=${INPUT_TYPE} \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
