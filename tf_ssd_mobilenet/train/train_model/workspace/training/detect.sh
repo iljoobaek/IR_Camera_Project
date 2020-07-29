45a0ac05e0843d21ec5d02d89943997a30d42399
Wei Chen <cweinone@gmail.com>
2020-07-29 14:28:30 -0400

    Detect cmu06

--- tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
@@ -2,8 +2,8 @@ export CUDA_VISIBLE_DEVICES=1
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU/Preprocessing/
-TARGET_PATH=setxx1
+TARGET_PATH=set06_L
 python detector.py ${BASE_PATH} \
                     ${TARGET_PATH} \
-                    ./detection/0713_flir_cmu05
+                    ./detection/0729_cmu06
 
