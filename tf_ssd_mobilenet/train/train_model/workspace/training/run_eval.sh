5fcc5d99068aa5761982f7a8f062aa45e889e922
Wei Chen <cweinone@gmail.com>
2020-08-06 14:28:19 -0400

    DEBUG

--- tf_ssd_mobilenet/train/train_model/workspace/training/run_eval.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/run_eval.sh
@@ -5,6 +5,6 @@ export CUDA_VISIBLE_DEVICES=1
 #                --checkpoint_dir=./training/train_faster_rcnn_inception_resnet_v2_FLIR_CMU00-05/ \
 #                --pipeline_config_path=./configs/faster_rcnn_inception_resnet_v2_atrous_coco.config
 
-python eval.py --eval_dir=./eval/eval_ssd_mobilenet_v1_FLIR_CMU00-05/ \
-                --checkpoint_dir=./training/eval_ssd_mobilenet_v1_FLIR_CMU00-05/ \
+python eval.py --eval_dir=./eval/eval_ssd_mobilenet_v1_FLIR_CMU00-05-selected/ \
+                --checkpoint_dir=./training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected/ \
                 --pipeline_config_path=./configs/ssd_mobilenet_v1.config
