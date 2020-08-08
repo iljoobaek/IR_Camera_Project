389ed804aec9f4d4096166841bbc3598bbfbda52
Wei Chen <cweinone@gmail.com>
2020-08-08 17:55:59 -0400

    Demo

--- tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
@@ -2,8 +2,8 @@ export CUDA_VISIBLE_DEVICES=1
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU/Preprocessing/
-TARGET_PATH=setxx2
+TARGET_PATH=setxx_cmu00-05-selected
 python detector.py ${BASE_PATH} \
                     ${TARGET_PATH} \
-                    ./detection/0808_FLIR_cmu00-05-selected
+                    ./detection/0808_cmu00-05-selected
 
--- tf_ssd_mobilenet/train/train_model/workspace/training/detector.py
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detector.py
@@ -224,7 +224,7 @@ class ObjectDetector(object):
 
 if __name__ == '__main__':
     checkpoint_path = os.path.join(dirname,
-        'training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected/model.ckpt-481232')
+        'training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected/model.ckpt-480879')
     pipeline_config_path = os.path.join(dirname,
         'training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected/pipeline.config')
     label_map_path = os.path.join(dirname,
