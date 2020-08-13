4b942bd0568477d725d31f8e4c93ab6b5e1ee914
Wei Chen <cweinone@gmail.com>
2020-08-13 17:02:50 -0400

    Start training

--- tf_ssd_mobilenet/train/train_model/workspace/training/run_training.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/run_training.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
-#python train.py --train_dir=./training/train_ssd_mobilenet_v1_CMU00-05-selected_0810/ \
-#                --pipeline_config_path=./configs/ssd_mobilenet_v1.config \
+python train.py --train_dir=./training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_0813/ \
+                --pipeline_config_path=./configs/ssd_mobilenet_v1.config \
 
-python train.py --train_dir=./training/train_faster_rcnn_inception_resnet_v2_CMU00-05-selected_0811/ \
-                --pipeline_config_path=./configs/faster_rcnn_inception_resnet_v2_atrous_coco.config \
+#python train.py --train_dir=./training/train_faster_rcnn_inception_resnet_v2_CMU00-05-selected_0811/ \
+#                --pipeline_config_path=./configs/faster_rcnn_inception_resnet_v2_atrous_coco.config \
