5486232f6e5d010ca46d4601993d256a90f43cc3
Wei Chen <cweinone@gmail.com>
2020-08-13 11:43:02 -0400

    DEBUG

--- tf_ssd_mobilenet/train/train_model/scripts/postprocessing/export_graph.sh
+++ tf_ssd_mobilenet/train/train_model/scripts/postprocessing/export_graph.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 INPUT_TYPE=image_tensor
 PIPELINE_CONFIG_PATH=../../workspace/training/training/train_ssd_mobilenet_v1_CMU00-05-selected_0810/pipeline.config
 TRAINED_CKPT_PREFIX=../../workspace/training/training/train_ssd_mobilenet_v1_CMU00-05-selected_0810/model.ckpt-352885
-EXPORT_DIR=../../workspace/training/exported_models/froze_graph_0813/
+EXPORT_DIR=../../workspace/training/exported_models/froze_graph_0813_1/
 python export_inference_graph.py \
     --input_type=${INPUT_TYPE} \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
