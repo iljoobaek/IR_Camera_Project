fb577dc630c29c25627676624b6f180b4d528d9a
Wei Chen <cweinone@gmail.com>
2020-08-16 15:26:41 -0400

    Detect chop

--- tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
@@ -2,8 +2,8 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU/Preprocessing/
-TARGET_PATH=set14_L
+TARGET_PATH=set14_L_chop
 python detector.py ${BASE_PATH} \
                     ${TARGET_PATH} \
-                    ./detection/0815_setmb
+                    ./detection/0815_setmb_chop
 
