8cf2476e23ada608a7d2ee9fa54f38f1f0bf3c6d
Wei Chen <cweinone@gmail.com>
2020-08-16 20:47:21 -0400

    detect right

--- tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
@@ -2,8 +2,8 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU/Preprocessing/
-TARGET_PATH=set14_L_chop
+TARGET_PATH=set14_R
 python detector.py ${BASE_PATH} \
                     ${TARGET_PATH} \
-                    ./detection/0815_setmb_chop
+                    ./detection/0815_setmb_r
 
