f52b94b847d2f977109f5568d2dd904ddc88727b
weichennone <cweinone@gmail.com>
2020-08-25 19:51:49 -0400

    update the model

--- tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
@@ -2,8 +2,8 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU/Preprocessing/
-TARGET_PATH=set14_R
+TARGET_PATH=set26_R
 python detector.py ${BASE_PATH} \
                     ${TARGET_PATH} \
-                    ./detection/0815_setmb_r
+                    ./detection/0824_set26mb_r
 
--- tf_ssd_mobilenet/train/train_model/workspace/training/run_training.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/run_training.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
-python train.py --train_dir=./training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_0814/ \
+python train.py --train_dir=./training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_0825/ \
                 --pipeline_config_path=./configs/ssd_mobilenet_v1.config \
 
 #python train.py --train_dir=./training/train_faster_rcnn_inception_resnet_v2_CMU00-05-selected_0811/ \
