1a3a80f4ab872acee58ae62a7a4e99070d7c1441
Wei Chen <cweinone@gmail.com>
2020-08-28 12:36:42 -0400

    Run detection on new model

--- tf_ssd_mobilenet/train/train_model/workspace/training/detector.py
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detector.py
@@ -228,9 +228,9 @@ class ObjectDetector(object):
 
 if __name__ == '__main__':
     checkpoint_path = os.path.join(dirname,
-        'training/train_ssd_mobilenet_v2_FLIR_CMU00-05-selected_all_0825/model.ckpt-300000')
+        'training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_0827_config1/model.ckpt-300000')
     pipeline_config_path = os.path.join(dirname,
-        'training/train_ssd_mobilenet_v2_FLIR_CMU00-05-selected_all_0825/pipeline.config')
+        'training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_0827_config1/pipeline.config')
     label_map_path = os.path.join(dirname,
         'training/cmudata_label_map.pbtxt')
     detector = ObjectDetector(checkpoint_path, pipeline_config_path, label_map_path)
