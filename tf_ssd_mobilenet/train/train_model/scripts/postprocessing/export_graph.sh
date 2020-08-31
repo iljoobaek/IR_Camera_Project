38860326db485921f542f47bd5d33334a219e313
Wei Chen <cweinone@gmail.com>
2020-08-31 13:36:06 -0400

    Cal confusion matrix

--- tf_ssd_mobilenet/train/train_model/scripts/postprocessing/export_graph.sh
+++ tf_ssd_mobilenet/train/train_model/scripts/postprocessing/export_graph.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 INPUT_TYPE=image_tensor
-PIPELINE_CONFIG_PATH=../../workspace/training/training/train_ssd_mobilenet_v1_CMU00-05-selected_0810/pipeline.config
-TRAINED_CKPT_PREFIX=../../workspace/training/training/train_ssd_mobilenet_v1_CMU00-05-selected_0810/model.ckpt-352885
-EXPORT_DIR=../../workspace/training/exported_models/froze_graph_0813_1/
+PIPELINE_CONFIG_PATH=../../workspace/training/training/train_ssd_mobilenet_v1_CMU00-05-selected_0813/pipeline.config
+TRAINED_CKPT_PREFIX=../../workspace/training/training/train_ssd_mobilenet_v1_CMU00-05-selected_0813/model.ckpt-350000
+EXPORT_DIR=../../workspace/training/exported_models/froze_graph_0831/
 python export_inference_graph.py \
     --input_type=${INPUT_TYPE} \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
