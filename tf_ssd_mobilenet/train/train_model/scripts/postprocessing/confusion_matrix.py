461281f5ea2b698e96da3e5930c2258286e3ee5f
Wei Chen <cweinone@gmail.com>
2020-11-19 18:43:35 -0500

    DEBUG

--- tf_ssd_mobilenet/train/train_model/scripts/postprocessing/confusion_matrix.py
+++ tf_ssd_mobilenet/train/train_model/scripts/postprocessing/confusion_matrix.py
@@ -225,9 +225,9 @@ def process_detections_2(detections_record1, detections_record2, categories):
                 detection_scores >= CONFIDENCE_THRESHOLD]
             detection_boxes = decoded_dict[standard_fields.DetectionResultFields.detection_boxes][
                 detection_scores >= CONFIDENCE_THRESHOLD]
-            all_detect_scores[image_index] = np.append(all_detect_scores[image_index], detection_scores)
-            all_detect_classes[image_index] = np.append(all_detect_classes[image_index], detection_classes)
-            all_detect_boxes[image_index] = np.append(all_detect_boxes[image_index], detection_boxes)
+            all_detect_scores[image_index] = np.append(all_detect_scores[image_index], detection_scores, 0)
+            all_detect_classes[image_index] = np.append(all_detect_classes[image_index], detection_classes, 0)
+            all_detect_boxes[image_index] = np.append(all_detect_boxes[image_index], detection_boxes, 0)
             print(all_detect_scores[image_index].shape, all_detect_classes[image_index].shape, all_detect_boxes[image_index].shape)
             exit()
 
