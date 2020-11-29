a559e2689964426524860d6f138eba8bec6559f9
Wei Chen <cweinone@gmail.com>
2020-11-29 15:10:11 -0500

    DEBUG

--- tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU/Preprocessing/
 TARGET_PATH=set26_R
-python detector.py ${BASE_PATH} \
+python detector_l_r.py ${BASE_PATH} \
                     ${TARGET_PATH} \
                     ./detection/0827_set26mb2_r_0907
 
