f6f4e5447d1b8f87881d5a9bc33693c4b81f20fe
Wei Chen <cweinone@gmail.com>
2020-12-08 11:32:51 -0500

    DEBUG

--- tf_ssd_mobilenet/train/train_model/workspace/training/detector_l_r.py
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detector_l_r.py
@@ -285,7 +285,7 @@ if __name__ == '__main__':
     #     print(example)
         img = cv2.imread(os.path.join(img_path, example + '.jpeg'))
         examplel = example.split('_')
-        examplel[2] = 'R'
+        examplel[2] = 'L'
         examplel = '_'.join(examplel)
         imgl = cv2.imread(os.path.join(img_path_l, examplel + '.jpeg'))
         # print(os.path.join(img_path_l, examplel + '.jpeg'))
