07f1bc273fac152319a1a113909eecf27e142820
Wei Chen <cweinone@gmail.com>
2020-12-08 11:34:33 -0500

    DEBUG

--- tf_ssd_mobilenet/train/train_model/workspace/training/detector_l_r.py
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detector_l_r.py
@@ -294,7 +294,7 @@ if __name__ == '__main__':
         if img is None:
             break
         height, width, channels = img.shape
-        img = detector.detect(imgl, img)
+        img = detector.detect(imgl, imgl)
         cv2.imwrite(os.path.join(output_dir, target + '_' + example + ".jpeg"), img)
         #timing
         frame_ctr = frame_ctr + 1
