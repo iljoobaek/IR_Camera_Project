15e3a88d04b4a753a299e2fe2b43ec49c99fbed2
Wei Chen <cweinone@gmail.com>
2020-12-08 11:21:47 -0500

    DEBUG

--- tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detect.sh
@@ -2,8 +2,8 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU/Preprocessing/
-TARGET_PATH=set25_L
+TARGET_PATH=set25_R
 python detector_l_r.py ${BASE_PATH} \
                     ${TARGET_PATH} \
-                    ./detection/0827_set25mb1_ll_1208
+                    ./detection/0827_set25mb1_rr_1208
 
--- tf_ssd_mobilenet/train/train_model/workspace/training/detector_l_r.py
+++ tf_ssd_mobilenet/train/train_model/workspace/training/detector_l_r.py
@@ -267,7 +267,7 @@ if __name__ == '__main__':
     base_path = sys.argv[1]
     target = sys.argv[2]
     img_path = os.path.join(base_path, 'Images', target)
-    img_path_r = os.path.join(base_path, 'Images', target.split('_')[0] + '_R')
+    img_path_l = os.path.join(base_path, 'Images', target.split('_')[0] + '_L')
     output_dir = sys.argv[3]
 
     frame_ctr = 0
@@ -287,7 +287,7 @@ if __name__ == '__main__':
         examplel = example.split('_')
         examplel[2] = 'R'
         examplel = '_'.join(examplel)
-        imgr = cv2.imread(os.path.join(img_path_r, examplel + '.jpeg'))
+        imgl = cv2.imread(os.path.join(img_path_l, examplel + '.jpeg'))
         # print(os.path.join(img_path_l, examplel + '.jpeg'))
         # exit()
         # print(img_path + example + '.jpeg')
