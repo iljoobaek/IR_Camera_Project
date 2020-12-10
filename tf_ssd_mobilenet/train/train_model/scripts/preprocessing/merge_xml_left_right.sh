d464772704eae37703e3aec23a9e1eaee66395b3
Wei Chen <cweinone@gmail.com>
2020-12-10 18:27:27 -0500

    DEBUG

--- tf_ssd_mobilenet/train/train_model/scripts/preprocessing/merge_xml_left_right.sh
+++ tf_ssd_mobilenet/train/train_model/scripts/preprocessing/merge_xml_left_right.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 BASE_PATH=/home/rtml/Documents/weichen/Datasets/CMU
 IMG_PATH=${BASE_PATH}/Preprocessing/Images/set_merged/
 OUT_PATH=${BASE_PATH}/Preprocessing/Annotations/set_merged/
-IN_PATH1=${BASE_PATH}/Preprocessing/Annotations/set00/
+IN_PATH1=${BASE_PATH}/Preprocessing/Annotations/set01/
 #IN_PATH2=${BASE_PATH}/Preprocessing/Annotations/annotation_traffic_cone_channelizer/
 #IN_PATH3=${BASE_PATH}/Preprocessing/Annotations/annotation_van_truck_bus/
 python merge_xml_left_right.py ${IMG_PATH} ${OUT_PATH} ${IN_PATH1}
