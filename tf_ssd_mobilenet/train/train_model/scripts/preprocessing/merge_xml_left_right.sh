a7385dbeeaafe63a32070aaf6a5289b35aabb530
Wei Chen <cweinone@gmail.com>
2020-12-10 18:05:59 -0500

    Create process script

---
+++ tf_ssd_mobilenet/train/train_model/scripts/preprocessing/merge_xml_left_right.sh
@@ -0,0 +1,9 @@
+export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
+# env: tfmobile_py2
+BASE_PATH=../Datasets/CMU/
+IMG_PATH=${BASE_PATH}/Preprocessing/Images/set_merged/
+OUT_PATH=${BASE_PATH}/Preprocessing/Annotations/set_merged/
+IN_PATH1=${BASE_PATH}/Preprocessing/Annotations/set00/
+#IN_PATH2=${BASE_PATH}/Preprocessing/Annotations/annotation_traffic_cone_channelizer/
+#IN_PATH3=${BASE_PATH}/Preprocessing/Annotations/annotation_van_truck_bus/
+python merge_xml_left_right.py ${IMG_PATH} ${OUT_PATH} ${IN_PATH1}
