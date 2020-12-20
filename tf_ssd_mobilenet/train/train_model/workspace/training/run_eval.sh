07f4b35e25a8dc33d14f82b6116c4a1f846d0423
Manoj <manojbhat09@gmail.com>
2020-12-20 11:23:45 -0500

    Train ROI

--- tf_ssd_mobilenet/train/train_model/workspace/training/run_eval.sh
+++ tf_ssd_mobilenet/train/train_model/workspace/training/run_eval.sh
@@ -9,6 +9,6 @@ export CUDA_VISIBLE_DEVICES=0
 #                --checkpoint_dir=./training/train_ssd_mobilenet_v2_FLIR_CMU00-05-selected_all_0907/ \
 #                --pipeline_config_path=./configs/ssd_mobilenet_v2.config
 
-python eval.py --eval_dir=./eval/eval_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_1219_homo/ \
-                --checkpoint_dir=./training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_1219_homo/ \
+python eval.py --eval_dir=./eval/eval_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_1220_roi/ \
+                --checkpoint_dir=./training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_1220_roi/ \
                 --pipeline_config_path=./configs/ssd_mobilenet_v1.config
