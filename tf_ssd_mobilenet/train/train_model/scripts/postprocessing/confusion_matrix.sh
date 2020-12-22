f1d7338fc19b590eb816c66df3fe90a67ba8d025
Manoj <manojbhat09@gmail.com>
2020-12-22 12:24:10 -0500

    evaluate ckp

--- tf_ssd_mobilenet/train/train_model/scripts/postprocessing/confusion_matrix.sh
+++ tf_ssd_mobilenet/train/train_model/scripts/postprocessing/confusion_matrix.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 BASE=../../workspace/training/
 
-python confusion_matrix.py --detections_record=${BASE}training/test_detections1.tfrecord \
+python confusion_matrix.py --detections_record=${BASE}training/test_detections.tfrecord \
                            --detections_record2=${BASE}training/test_detections1.tfrecord \
                            --label_map=${BASE}training/cmudata_label_map.pbtxt \
                            --output_path=${BASE}eval/confusion_matrix.csv
--- tf_ssd_mobilenet/train/train_model/scripts/postprocessing/export_graph.sh
+++ tf_ssd_mobilenet/train/train_model/scripts/postprocessing/export_graph.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PYTHONPATH:../../models/research/:../../models/research/slim
 
 INPUT_TYPE=image_tensor
-PIPELINE_CONFIG_PATH=../../workspace/training/training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_0813/pipeline.config
-TRAINED_CKPT_PREFIX=../../workspace/training/training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_0813/model.ckpt-350000
-EXPORT_DIR=../../workspace/training/exported_models/froze_graph_0831/
+PIPELINE_CONFIG_PATH=../../workspace/training/training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_1220_roi/pipeline.config
+TRAINED_CKPT_PREFIX=../../workspace/training/training/train_ssd_mobilenet_v1_FLIR_CMU00-05-selected_all_1220_roi/model.ckpt-300000
+EXPORT_DIR=../../workspace/training/exported_models/froze_graph_1220/
 python export_inference_graph.py \
     --input_type=${INPUT_TYPE} \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
